fb81c19e458a6b21c856af403b9025b307747550
nbogie <neillbogie@googlemail.com>
2022-09-06 19:41:55 +0100

    remove express startup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { Telegraf } from "telegraf";
-
 /*
   TELEGRAM_BOT_TOKEN is an environment variable
   that should be configured on Railway
@@ -9,27 +8,34 @@ import { Telegraf } from "telegraf";
 if (!process.env.TELEGRAM_BOT_TOKEN) throw new Error("Please add a bot token");
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.start(ctx => ctx.reply("Welcome"));
-bot.hears("hello", ctx => {
+console.log("registering bot's listeners");
+bot.start((ctx) => ctx.reply("Welcome"));
+bot.hears("hello", (ctx) => {
+  console.log("got hello");
   ctx.reply("Hello to you too!");
 });
-bot.hears("date", ctx => {
+bot.hears("date", (ctx) => {
+  console.log("got date");
   ctx.reply("Date and time is: " + new Date());
 });
 
 bot.launch();
+console.log("bot has been launched (no express) " + new Date());
+function startExpress() {
+  const app = express();
+  const port = process.env.PORT || 3333;
 
-const app = express();
-const port = process.env.PORT || 3333;
+  app.use(bodyParser.json());
+  app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
+  app.use(bodyParser.text({ type: "text/html" }));
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+  app.get("/", async (req, res) => {
+    res.json({ Hello: "World" });
+  });
 
-app.get("/", async (req, res) => {
-  res.json({ Hello: "World" });
-});
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+}
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// startExpress()
